05efbccf9bd5dd6f6600739e39bbe4ddd1429459
Neli Harbuzava <ineliharbuzava@gmail.com>
2022-09-03 22:31:30 +0300

    pset 6 mario less

---
+++ pset6/sentimental-mario-less/mario.py
@@ -0,0 +1,22 @@
+def main():
+    height = get_height()
+
+    for i in range(height):
+        grids = i + 1
+        spaces = height - grids
+        print(" " * spaces + "#" * grids)
+
+
+def get_height():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if height >= 1 and height <= 8:
+                break
+        except ValueError:
+            continue
+
+    return height
+
+
+main()
